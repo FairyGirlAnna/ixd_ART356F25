cfb6e466945e67a0352c277b8124069296fa27bd
Anna <dascam9001@gmail.com>
2025-11-09 23:09:54 -0800

    Update sketch.js

--- Assignments/Project2/sketch.js
+++ Assignments/Project2/sketch.js
@@ -1,24 +1,33 @@
-let data;
-
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dR9yt211Fo9YH2cZRGeELLyKLGKWLh0W&limit=5");
-xhr.done(function(data) { console.log("success got data", data); });
+let api = "";
+let data = "";
+let storeddata = "";
+let searchquery = "ryan+gosling"
 
 let title = "Pick a category!";
 let titles = ["Dog", "Cat", "Spongebob", "Simpsons", "Halloween", "Christmas"];
-
-function preload(){
-  data = loadTable(url, "csv", "header");
-}
 
 function setup() {
   createCanvas(600, 600);
   textAlign(CENTER, CENTER);
   textWrap(WORD);
+  
+}
+
+function search(searchquery){
+  let api = "http://api.giphy.com/v1/gifs/search?q=" + searchquery +"&api_key=dR9yt211Fo9YH2cZRGeELLyKLGKWLh0W&limit=5";
+  loadJSON(api, gotData);
+  //api.done(function(data) { console.log("success got data", data); });
+  let storeddata = data;
+  createImg(storeddata.data[0].images.original.url);
+  
+}
+
+function gotData(){
+  createImg(data.data[0].images.original.url);
 }
 
 function draw() {
   background(10, 50, 30);
-  // print(mouseX, mouseY);
   textSize(12);
   for (let index = 0; index < titles.length; index++) {
     fill(0);
@@ -28,7 +37,6 @@ function draw() {
     
   }
 
-  //title
   textSize(32);
   if (title == "Pick a category!"){
     text (title, width/2, 40); 
@@ -43,74 +51,37 @@ function draw() {
   switch (title) {
     case "Dog":
       
-      text ("GIFS", width/2, 450);
+      text ("Top tags for " + title + " GIFS:", width/2, 450);
       break;
     
     case "Cat":
       
-      text ("GIFS", width/2, 450);
+      text ("Top tags for " + title + " GIFS:", width/2, 450);
       break;
 
     case "Spongebob":
       
-      text ("GIFS", width/2, 450);
+      text ("Top tags for " + title + " GIFS:", width/2, 450);
       break;
 
     case "Simpsons":
       
-      text ("GIFS", width/2, 450);
+      text ("Top tags for " + title + " GIFS:", width/2, 450);
       break;
     
     case "Halloween":
       
-      text ("GIFS", width/2, 450);
+      text ("Top tags for " + title + " GIFS:", width/2, 450);
       break;
 
     case "Christmas":
       
-      text ("GIFS", width/2, 450);
+      text ("Top tags for " + title + " GIFS:", width/2, 450);
       break;
 
     default:
       break;
   }
-
-  // textSize(16);
-  // if (data){
-  //   let rowcount = data.rows.length;
-  //   let names = data.getColumn("Food");
-  //   let shortnames = data.getColumn("Short Name");
-  //   let cal = data.getColumn(datatype);
-  //   circlediameter.length = 0;
-  //   // prep variables & clear array when finished
-
-  //   for (let i = 0; i < datatypes.length-2; i++) {
-  //     text(names[i-1], 100*i, 250, 1);
-  //     text(cal[i-1], 100*i, 350, 1);
-  //     fill(255);
-      
-  //     let modifier = 100/max(cal); 
-  //     circlediameter.push(cal[i] * modifier);
-  //     circle((100*i)+100, 450, circlediameter[i]);
-      
-  // }
-  // }
-
-  //display bars
-  // if (data){
-  //   let rowcount = data.rows.length
-  //   let names = data.getColumn("Food")
-  //   let cal = data.getColumn(datatype)
-
-  //   for (let i = 0; i < rowcount; i++) {
-  //     textSize(24);
-  //     text(names[i], 50, (60*i) + 90,)
-  //     rect(50, (60*i) + 100, cal[i], 10)
-
-  //     textSize(16);
-  //     text(cal[i], (parseInt(cal[i])+25), 120+(60*i));
-  //   }
-  // }
 }
 
 
@@ -121,4 +92,5 @@ function mousePressed(){
       title = titles[tempselection];
     }
   }
+  search(storeddata);
 }
